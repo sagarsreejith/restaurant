1fa0e55d57f331100149eb7259ff286e626c1b94
Srividya <srividya@gmt.com.kw>
2019-07-13 16:10:27 +0300

    msg

--- extensions/slideshow/views/slideshow.php
+++ extensions/slideshow/views/slideshow.php
@@ -1,29 +1,4 @@
-<?php if ($display_slides) { ?>
-<div id="slider">
-	<div class="flexslider">
-		<ul class="slides">
-			<?php if (!empty($slides)) { ?>
-				<?php foreach ($slides as $slide) { ?>
-					<?php if (isset($slide['image_src'])) { ?>
-						<li>
-							<div class="slider-caption">
-								<?php echo $slide['caption']; ?>
-							</div>
+<div class="background_images">
 
-							<img src="<?php echo $slide['image_src']; ?>" />
-						</li>
-					<?php } ?>
-				<?php } ?>
-			<?php } else { ?>
-				<li></li>
-			<?php } ?>
-	  	</ul>
-	</div>
+<img src="assets/images/slide2.jpg">
 </div>
-<script type="text/javascript"><!--
-	$('.flexslider').flexslider({
-		prevText: '',
-		nextText: ''
-	});
-//--></script>
-<?php } ?>
