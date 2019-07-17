ad6f07d201dffbbe6a5f77e21fad1e8510f1424a
Srividya <srividya@gmt.com.kw>
2019-07-17 16:19:42 +0300

    msg

--- extensions/slideshow/views/slideshow.php
+++ extensions/slideshow/views/slideshow.php
@@ -1,4 +1,112 @@
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+
+<style>
+    .panel-heading .accordion-toggle:after {
+   
+    font-family: 'Glyphicons Halflings'; 
+    content: "\e114";    
+    float: right;        
+    color: #5d5c5c;
+    font-size: 14px;
+}        
+
+.panel-heading .accordion-toggle.collapsed:after {
+    
+    content: "\e080";  
+}
+.accordion-toggle{
+    font-size: 24px;
+    color: #000;
+    font-weight: 800;
+}
+.accordion-toggle:hover{
+text-decoration:none;
+}
+    </style>
+
 <div class="background_images">
 
 <img src="assets/images/slide2.jpg">
+</div>
+
+<div class="allformst">
+    <div class="container">
+        <div class="">
+        <ul class="nav nav-tabs tabstab">
+                <li class="fts active">
+                    <a href="#Menu" aria-controls="flight" role="tab" data-toggle="tab">
+                        <!-- <span class="iconcmn icnflt"></span> --><strong>Menu</strong></a>
+                </li>
+                <li class="ht ">
+                    <a href="#reservation" aria-controls="hotel" role="tab" data-toggle="tab"> <strong>Reservation</strong></a>
+                </li>
+              
+               
+         </ul>
+        <div class="secndblak">
+                <div class="tab-content custmtab">
+                            <p class="text-center">Welcome</p>
+                    <div class="tab-pane" id="Menu">
+               
+<div class = "">
+
+  
+  <div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+          Collapsible Group Item #1
+        </a>
+      </h4>
+    </div>
+    <div id="collapseOne" class="panel-collapse collapse">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+          Collapsible Group Item #2
+        </a>
+      </h4>
+    </div>
+    <div id="collapseTwo" class="panel-collapse collapse">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+          Collapsible Group Item #3
+        </a>
+      </h4>
+    </div>
+    <div id="collapseThree" class="panel-collapse collapse">
+      <div class="panel-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
+  
+  
+</div> <!-- end container -->
+
+
+                    </div>
+                    <div class="tab-pane" id="reservation">
+                    reservations
+                    </div>
+                </div>
+        </div>
+    </div>
 </div>
