60691769e6a36ba489f888f92dbdebf4fd9803ef
Srividya <srividya@gmt.com.kw>
2019-07-24 11:33:25 +0300

    lugma24

---
+++ system/tastyigniter/logs/logs.php
@@ -0,0 +1,27 @@
+ERROR - 2019-07-24 11:19:21 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:19:21 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:19:22 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:19:22 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:19:23 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:19:23 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:19:23 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:19:23 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:19:24 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:19:24 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:19:24 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:19:24 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:19:24 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:19:24 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:20:30 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:20:30 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
+ERROR - 2019-07-24 11:21:14 --> Query error: Table 'rest.dw2rtzns5_governates' doesn't exist - Invalid query: SELECT *
+FROM `dw2rtzns5_governates`
+ERROR - 2019-07-24 11:21:14 --> Severity: Error --> Call to a member function num_rows() on boolean D:\xampp\htdocs\out-rest\system\tastyigniter\models\Reviews_model.php 305
