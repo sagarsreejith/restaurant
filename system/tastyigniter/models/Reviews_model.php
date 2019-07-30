902f703e4173403e48b5b5893de48c82adcee3d7
Sagar Sreejith <sreejith.sagar@gmail.com>
2019-07-30 10:38:47 +0300

    Disabled functinalty enabed on search

--- system/tastyigniter/models/Reviews_model.php
+++ system/tastyigniter/models/Reviews_model.php
@@ -297,6 +297,20 @@ class Reviews_model extends TI_Model {
 		}
 	}
 
+	public function getAllBranchLocation(){
+		$this->db->from('locations');
+		$this->db->select('location_city');
+		$this->db->where('location_status', 1);
+		$query = $this->db->get();
+		$result = array();
+
+		if ($query->num_rows() > 0) {
+			$result = $query->result_array();
+		}
+
+		return $result;
+	 }
+
 	public function getAllGovernates(){
 		$this->db->from('governates');
 		//$this->db->join('governate_areas', 'governate_areas.govr_id = governates.govr_id', 'inner');
@@ -310,19 +324,31 @@ class Reviews_model extends TI_Model {
 
 	 public function populateGovernateArryay($governates = array(), $count){
 		$count = 0;
+		$brnaches_location = $this->getAllBranchLocation();
 		foreach ($governates as $governate){
 			$this->db->from('governate_areas');
 			$this->db->where('govr_id', $governate['govr_id']);
 			$query = $this->db->get();
 			if ($query->num_rows() > 0) {
 				$governates[$count]['is_governate'] = true;
-				$governates[$count]['areas'] = $query->result_array();
+				$governates[$count]['areas'] = $this->updateAreas($query->result_array(), $brnaches_location);
 			}
 			$count++;
 		}
 
 		return $governates;
 
+	 }
+
+	 public function updateAreas($areas_array, $branch_locations){
+		 $final_Array = [];
+		foreach ($areas_array as $area){
+			// $branches = array_column($branch_locations, 'location_city');
+			// $disable_status = array_search($area['govr_area_name_en'], $branches);
+			$area['is_disabled'] = in_array($area['govr_area_name_en'], array_column($branch_locations, 'location_city'));
+			array_push($final_Array, $area);
+		}
+		return $final_Array;
 	 }
 }
 
