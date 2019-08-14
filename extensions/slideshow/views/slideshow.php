ebf7bcf2ab004a549a6ae4a3cc34564ed4cc8a0e
Srividya <srividya@gmt.com.kw>
2019-08-14 11:17:52 +0300

    14th

--- extensions/slideshow/views/slideshow.php
+++ extensions/slideshow/views/slideshow.php
@@ -1,7 +1,30 @@
+<style>
+/* .background_images:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    z-index: 10;
+    width: 100%;
+    height: 80px;
+    top: 40px;
+    background: url(assets/images/down.png) repeat-x;
+} */
+.background_images:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    z-index: 10;
+    width: 100%;
+    height: 80px;
+    bottom:-55px;
+    background: url(assets/images/up1.png) repeat-x;
+}
 
+</style>
 <div class="background_images fadeIn">
 
-<img src="assets/images/bg9.jpg">
+<img src="assets/images/baoli.jpg">
+<!-- <img src="assets/images/d2.jpg"> -->
 </div>
 
 <!-- <section class="page-section-pb our-menuss">
--- system/tastyigniter/logs/logs.php
+++ system/tastyigniter/logs/logs.php
@@ -1,3 +1,13 @@
 ERROR - 2019-08-10 17:20:04 --> Query error: Unknown column 'building_no' in 'field list' - Invalid query: INSERT INTO `dw2rtzns5_addresses` (`address_1`, `address_2`, `city`, `state`, `postcode`, `building_no`, `avenue`, `apt_no`, `floor`, `office`, `country_id`) VALUES ('111', 'sss', 'Sharq', 'Kuwait City (Capital)', '123456', '1', '1', '1', '1', '', '114')
 ERROR - 2019-08-10 17:22:38 --> Query error: Unknown column 'building_no' in 'field list' - Invalid query: INSERT INTO `dw2rtzns5_addresses` (`address_1`, `address_2`, `city`, `state`, `postcode`, `building_no`, `avenue`, `apt_no`, `floor`, `office`, `country_id`) VALUES ('5', 'salmiya', 'Sharq', 'Kuwait City (Capital)', '123456', '1', '', '12', '3', '', '114')
 ERROR - 2019-08-10 17:24:22 --> Query error: Unknown column 'building_no' in 'field list' - Invalid query: INSERT INTO `dw2rtzns5_addresses` (`address_1`, `address_2`, `city`, `state`, `postcode`, `building_no`, `avenue`, `apt_no`, `floor`, `office`, `country_id`) VALUES ('12', '', 'Sharq', 'Kuwait City (Capital)', '123456', '', '', '', '', '', '114')
+ERROR - 2019-08-14 07:22:30 --> Unable to connect to the database
+ERROR - 2019-08-14 07:22:33 --> Unable to connect to the database
+ERROR - 2019-08-14 10:55:22 --> 404 Page Not Found: 
+ERROR - 2019-08-14 10:56:23 --> 404 Page Not Found: 
+ERROR - 2019-08-14 10:59:36 --> 404 Page Not Found: 
+ERROR - 2019-08-14 11:06:08 --> 404 Page Not Found: 
+ERROR - 2019-08-14 11:12:35 --> 404 Page Not Found: 
+ERROR - 2019-08-14 11:12:35 --> 404 Page Not Found: 
+ERROR - 2019-08-14 11:12:36 --> 404 Page Not Found: 
+ERROR - 2019-08-14 11:15:18 --> 404 Page Not Found: 
