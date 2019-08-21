ff59b23ba97dfa4a30c72779473dacb44f1e0167
Sagar Sreejith <sreejith.sagar@gmail.com>
2019-08-21 08:49:33 +0300

    Changes regarding the track page

--- main/controllers/Track.php
+++ main/controllers/Track.php
@@ -5,16 +5,6 @@ class Track extends Main_Controller {
 	public function index() {
         $this->lang->load('home');
         
-        
-        if ($this->config->item('maps_api_key')) {
-            $map_key = '&key=' . $this->config->item('maps_api_key');
-        } else {
-            $map_key = '';
-        }
-        
-        $this->template->setScriptTag('https://maps.googleapis.com/maps/api/js?v=3' .$map_key.'&libraries=geometry,places', 'google-maps-js', '104330');
-        $this->template->setTitle($this->lang->line('text_heading'));
-
 		$this->template->render('track-my-order');
 	}
 }
